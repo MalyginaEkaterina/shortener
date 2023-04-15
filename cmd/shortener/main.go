e6c6b41639b2235a75220e1b6ebaab8170dce605
MalyginaEkaterina <katty2007v@yandex.ru>
2023-04-15 23:52:51 +0300

    Инкремент 19

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,8 +1,32 @@
 package main
 
-import "github.com/MalyginaEkaterina/shortener/internal/app"
+import (
+	"fmt"
+	"github.com/MalyginaEkaterina/shortener/internal/app"
+)
+
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
 
 func main() {
+	if buildVersion != "" {
+		fmt.Printf("Build version: %s\n", buildVersion)
+	} else {
+		fmt.Printf("Build version: N/A\n")
+	}
+	if buildDate != "" {
+		fmt.Printf("Build date: %s\n", buildDate)
+	} else {
+		fmt.Printf("Build date: N/A\n")
+	}
+	if buildCommit != "" {
+		fmt.Printf("Build commit: %s\n", buildCommit)
+	} else {
+		fmt.Printf("Build commit: N/A\n")
+	}
 	app.Start()
 }
 
